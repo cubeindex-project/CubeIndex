4607974bca02ced3424a2c02fbe23b7c4e948f42
Saterz_ <136126641+Saterz@users.noreply.github.com>
2025-04-16 11:01:30 +0200

    feat: add @vercel/speed-insights dependency and inject speed insights in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,10 @@
 	import Navbar from "$lib/components/navbar.svelte";
 	import Footer from "$lib/components/footer.svelte";
 	import { injectAnalytics } from "@vercel/analytics/sveltekit";
-	
+	import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+
+	injectAnalytics()
+	injectSpeedInsights();
 	let { children } = $props();
 </script>
 

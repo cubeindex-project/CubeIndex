445050b9b491323c18211f0cfa524a839e62ac06
Saterz_ <136126641+Saterz@users.noreply.github.com>
2025-05-20 12:14:34 +0100

    refactor: simplify Discord signup function by removing redirect options

--- src/routes/(auth)/auth/signup/+page.svelte
+++ src/routes/(auth)/auth/signup/+page.svelte
@@ -32,12 +32,7 @@
     }
 
     async function signUpDiscord() {
-        await supabase.auth.signInWithOAuth({
-            provider: 'discord',
-            options: {
-                redirectTo: `https://cube-index-2nr0s9wpc-saterz-studio.vercel.app/auth/callback`,
-            },
-        });
+        await supabase.auth.signInWithOAuth({ provider: 'discord' });
     }
 
     function togglePasswordVisibility() {

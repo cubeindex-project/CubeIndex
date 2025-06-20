20edf3c103b0d4e68fef00f53c81c0d3a7528e21
Saterz_ <tilouc93@gmail.com>
2025-06-20 14:46:04 +0100

    fix: update redirect URLs in Discord authentication to use window.location.origin

--- src/routes/(auth)/auth/signup/discord/+server.ts
+++ src/routes/(auth)/auth/signup/discord/+server.ts
@@ -1,11 +1,11 @@
 import type { RequestHandler } from "./$types";
 import { redirect } from "@sveltejs/kit";
 
-export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
+export const GET: RequestHandler = async ({ locals: { supabase } }) => {
   const { data } = await supabase.auth.signInWithOAuth({
     provider: "discord",
     options: {
-      redirectTo: url.origin + "/auth/signup-callback",
+      redirectTo: `${window.location.origin}/auth/signup-callback`,
     },
   });
 

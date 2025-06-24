bea40a7257010381594358aacfdaeb7bfeb4ff7e
Saterz_ <tilouc93@gmail.com>
2025-06-24 12:51:38 +0100

    feat: Added submission page for CubeIndex Championship (#42)
    * feat: implement submission form for CubeIndex Championship
    
    * fix: remove unused data logging in submission action

---
+++ src/routes/(public)/championship/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  let { data }: { data: PageData } = $props();
+
+  import InConstruction from "$lib/components/inConstruction.svelte";
+</script>
+
+<InConstruction />

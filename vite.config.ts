1fcd741547fd111ba4333fe419e1022c19a9dc03
Saterz_ <tilouc93@gmail.com>
2025-06-25 18:56:57 +0100

    fix: remove unused error localization file and simplify Vite plugin strategy

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     paraglideVitePlugin({
       project: "./project.inlang",
       outdir: './src/lib/paraglide',
-      strategy: ["preferredLanguage", "url", "cookie", "globalVariable", "baseLocale"],
+      strategy: ["preferredLanguage", "baseLocale"],
     }),
     devtoolsJson(),
     tailwindcss(),

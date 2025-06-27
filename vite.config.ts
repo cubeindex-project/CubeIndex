f4e11f7e5f2d69015c42ed6473ea3ec4e09811c7
Saterz_ <tilouc93@gmail.com>
2025-06-27 09:50:38 +0100

    fix: update overview user tab label and enhance Vite plugin strategy

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     paraglideVitePlugin({
       project: "./project.inlang",
       outdir: './src/lib/paraglide',
-      strategy: ["preferredLanguage", "baseLocale"]
+      strategy: ["preferredLanguage", "url", "baseLocale"]
     }),
     devtoolsJson(),
     tailwindcss(),

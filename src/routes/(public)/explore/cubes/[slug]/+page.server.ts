c8dcbbb4758dd57f0dbaad3b8f9b8c4cae777aa1
Saterz_ <tilouc93@gmail.com>
2025-07-03 10:03:27 +0100

    feat: implement csr of cubes on cube detail page

--- src/routes/(public)/explore/cubes/[slug]/+page.server.ts
+++ src/routes/(public)/explore/cubes/[slug]/+page.server.ts
@@ -1,10 +1,7 @@
-import { supabase } from "$lib/supabaseClient";
-import { error, type Actions, fail } from "@sveltejs/kit";
+import { type Actions, fail } from "@sveltejs/kit";
 import { configCatClient } from "$lib/configcatClient";
 
-export const load = async ({ params }) => {
-  const { slug } = params;
-
+export const load = async () => {
   let databaseAvailability: boolean = true;
   let cubesAvailability: boolean = true;
 
@@ -15,109 +12,6 @@ export const load = async ({ params }) => {
   configCatClient.getValueAsync("cubes", false).then((value) => {
     cubesAvailability = value;
   });
-
-  if (databaseAvailability || cubesAvailability) {
-    const { data: cube, error: cubeError } = await supabase
-      .from("cube_models")
-      .select("*")
-      .eq("slug", slug)
-      .single();
-
-    if (cubeError)
-      throw error(
-        500,
-        `Failed to fetch cube with slug "${slug}": ${cubeError.message}`
-      );
-
-    let relatedCube = null;
-    if (cube.version_type !== "Base" || cube.modded === true) {
-      const { data, error: relatedError } = await supabase
-        .from("cube_models")
-        .select("*")
-        .eq("slug", cube.related_to)
-        .single();
-
-      if (relatedError)
-        throw error(
-          500,
-          `Failed to fetch related cube with slug "${cube.related_to}": ${relatedError.message}`
-        );
-      relatedCube = data;
-    }
-
-    const { data: sameSeries, error: seriesError } = await supabase
-      .from("cube_models")
-      .select("*")
-      .match({
-        series: cube.series,
-        version_type: "Base",
-      })
-      .neq("model", cube.model);
-
-    if (seriesError)
-      throw error(
-        500,
-        `Failed to fetch cube with slug "${slug}": ${seriesError.message}`
-      );
-
-    const { data: cubeTrims, error: trimsErr } = await supabase
-      .from("cube_models")
-      .select("*")
-      .eq("related_to", slug);
-
-    if (trimsErr)
-      throw error(
-        500,
-        `Failed to fetch cube trims related to slug "${slug}": ${trimsErr.message}`
-      );
-
-    const { data: vendor_links, error: vendorError } = await supabase
-      .from("cube_vendor_links")
-      .select("*")
-      .eq("cube_slug", cube.slug);
-
-    if (vendorError)
-      throw error(
-        500,
-        `Failed to fetch vendor links for cube "${cube.slug}": ${vendorError.message}`
-      );
-
-    const { data: profiles, error: profilesError } = await supabase
-      .from("profiles")
-      .select("id, username");
-
-    if (profilesError)
-      throw error(500, `Failed to fetch profiles: ${profilesError.message}`);
-
-    const { data: user_ratings, error: ratingsError } = await supabase
-      .from("user_ratings")
-      .select("*")
-      .eq("cube_slug", cube.slug);
-
-    if (ratingsError)
-      throw error(500, `Failed to fetch profiles: ${ratingsError.message}`);
-
-    const { data: cubeUserCount, error: userCubesErr } = await supabase
-      .from("user_cubes")
-      .select("*")
-      .eq("cube", cube.slug);
-
-    if (userCubesErr)
-      throw error(500, `Failed to fetch profiles: ${userCubesErr.message}`);
-
-    return {
-      cube,
-      cubeTrims,
-      relatedCube,
-      sameSeries,
-      vendor_links,
-      profiles,
-      user_ratings,
-      databaseAvailability,
-      cubesAvailability,
-      cubeUserCount,
-    };
-  }
 
   return { databaseAvailability, cubesAvailability };
 };

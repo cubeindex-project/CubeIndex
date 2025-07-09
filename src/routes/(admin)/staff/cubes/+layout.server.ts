da193f537a0164739c6443495a5d5a4f8a12e6b6
Saterz_ <tilouc93@gmail.com>
2025-07-09 17:43:31 -0400

    feat: Update routing and access control for staff and admin sections

--- src/routes/(admin)/staff/cubes/+layout.server.ts
+++ src/routes/(admin)/staff/cubes/+layout.server.ts
@@ -1,7 +1,7 @@
 import type { LayoutServerLoad } from "./$types";
-import { error, redirect } from "@sveltejs/kit";
+import { redirect, error } from "@sveltejs/kit";
 
-export const load = (async ({ locals: { supabase, user } }) => {
+export const load = (async ({ locals: { user, supabase } }) => {
   if (!user) redirect(303, "/auth/login");
 
   const { data: profile, error: err } = await supabase
@@ -12,7 +12,6 @@ export const load = (async ({ locals: { supabase, user } }) => {
 
   if (err) throw error(500, err.message);
 
-  if (profile.role === "User") redirect(303, "/");
-
-  return { profile, user };
+  if (profile.role !== "Admin" || profile.role !== "Database Manager")
+    redirect(303, "/staff/dashboard");
 }) satisfies LayoutServerLoad;

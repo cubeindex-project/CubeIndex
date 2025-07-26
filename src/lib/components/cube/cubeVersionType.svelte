703b9b2e7ccc2f6ec1f7d77f101e8110b24c6cd3
Saterz_ <tilouc93@gmail.com>
2025-07-26 16:12:57 -0400

    fix: fixed the visual presentation of version types by ensuring consistent text color with background

--- src/lib/components/cube/cubeVersionType.svelte
+++ src/lib/components/cube/cubeVersionType.svelte
@@ -4,19 +4,19 @@
 
 {#if version_type === "Base"}
   <span
-    class="px-2 py-1 rounded bg-primary text-xs font-semibold"
+    class="px-2 py-1 rounded bg-primary text-primary-content text-xs font-semibold"
   >
     <span class="font-clash uppercase">Base</span>
   </span>
 {:else if version_type === "Limited"}
   <span
-    class="px-2 py-1 rounded bg-accent text-neutral text-xs font-semibold"
+    class="px-2 py-1 rounded bg-accent text-accent-content text-xs font-semibold"
   >
     <span class="font-clash uppercase">Limited Edition</span>
   </span>
 {:else}
   <span
-    class="px-2 py-1 rounded bg-secondary text-xs font-semibold"
+    class="px-2 py-1 rounded bg-secondary text-secondary-content text-xs font-semibold"
   >
     <span class="font-clash uppercase">Trim</span>
   </span>

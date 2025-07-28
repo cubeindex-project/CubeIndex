48bf5024536286fac508d23200dce350aaea5baf
Saterz_ <tilouc93@gmail.com>
2025-07-28 18:37:01 -0400

    fix: bind currentPage in Pagination component to fix pagination bug in user ratings page

--- src/routes/(private)/user/[id]/ratings/+page.svelte
+++ src/routes/(private)/user/[id]/ratings/+page.svelte
@@ -38,7 +38,7 @@
           showCubeDetails={true}
         />
       {/each}
-      <Pagination {currentPage} {totalPages} />
+      <Pagination bind:currentPage={currentPage} {totalPages} />
     {:else}
       <div
         class="col-span-full flex flex-col items-center justify-center py-20"

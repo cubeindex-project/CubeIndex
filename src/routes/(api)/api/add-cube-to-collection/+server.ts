b77af31f70bdc08191fcc87d57a18960560aba8b
Saterz_ <tilouc93@gmail.com>
2025-07-31 12:08:12 -0400

    refactor: change insert to upsert in add-cube-to-collection API for improved error handling

--- src/routes/(api)/api/add-cube-to-collection/+server.ts
+++ src/routes/(api)/api/add-cube-to-collection/+server.ts
@@ -37,7 +37,7 @@ export const POST: RequestHandler = async ({ request, locals }) => {
 
   const { error: userCubesErr } = await locals.supabase
     .from("user_cubes")
-    .insert({
+    .upsert({
       username: profile.username,
       cube,
       quantity,
@@ -49,17 +49,6 @@ export const POST: RequestHandler = async ({ request, locals }) => {
     })
     .select();
 
-  if (
-    userCubesErr?.message ===
-    'duplicate key value violates unique constraint "user_cubes_pkey"'
-  )
-    return json(
-      {
-        success: false,
-        error: "You already added this cube to your collection!",
-      },
-      { status: 500 }
-    );
   if (userCubesErr)
     return json(
       { success: false, error: "An error occured: " + userCubesErr.message },

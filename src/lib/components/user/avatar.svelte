8e0526e11fff6a2a152b5ffc68491d036ece584e
Saterz_ <tilouc93@gmail.com>
2025-08-02 13:46:00 -0400

    feat: add user avatar component
    - Introduced a new avatar component that displays user profile pictures or a placeholder with the user's initial.
    - The component supports different sizes (small, medium, large) for responsive design.

---
+++ src/lib/components/user/avatar.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import type { Profiles } from "../types/profile";
+
+  const { profile, size }: { profile: Profiles; size: "sm" | "md" | "lg" } =
+    $props();
+</script>
+
+{#if profile.profile_picture}
+  <img
+    src={profile.profile_picture}
+    alt="Avatar"
+    class="rounded-2xl border-4 border-primary bg-black object-cover transition-transform duration-200
+    {size === 'lg'
+      ? 'size-32 sm:size-64'
+      : size === 'md'
+        ? 'size-24 sm:size-24'
+        : 'size-14 sm:size-14'}"
+  />
+{:else}
+  <div class="avatar avatar-placeholder">
+    <div
+      class="bg-base-300 rounded-2xl border-4 border-primary
+      {size === 'lg'
+        ? 'size-32 sm:size-64'
+        : size === 'md'
+          ? 'size-24 sm:size-24'
+          : 'size-14 sm:size-14'}"
+    >
+      <span
+        class="{size === 'lg'
+          ? 'text-9xl'
+          : size === 'md'
+            ? 'text-6xl'
+            : 'text-2xl'} uppercase font-clash"
+      >
+        {profile.username.charAt(0)}
+      </span>
+    </div>
+  </div>
+{/if}

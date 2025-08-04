280fab6a5d7d0ccb264c7656afa18bd1ae564e8d
Saterz_ <tilouc93@gmail.com>
2025-08-04 15:12:48 -0400

    feat: add display name functionality to user signup and profile creation
    - Introduced a new field for display name in the signup form, requiring a minimum length of 4 characters.
    - Updated the profile creation logic to include display name alongside username in the database.
    - Enhanced the user interface to accommodate the new display name input, improving user experience during signup.

--- src/routes/(auth)/auth/signup/+page.server.ts
+++ src/routes/(auth)/auth/signup/+page.server.ts
@@ -4,6 +4,7 @@ import type { Actions } from "./$types";
 export const actions: Actions = {
   signup: async ({ request, locals: { supabase } }) => {
     const formData = await request.formData();
+    const display_name = formData.get("display_name") as string;
     const username = formData.get("username") as string;
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
@@ -18,9 +19,13 @@ export const actions: Actions = {
       return fail(400, { error: "Passwords do not match" });
     if (!emailRegex.test(email))
       return fail(400, { error: "Please enter a valid email address" });
-    if (username.length <= 2 || username.length >= 12)
+    if (display_name.length <= 3)
       return fail(400, {
-        error: "Username must be between 2 and 12 characters",
+        error: "The display name must have more than 3 characters",
+      });
+    if (username.length <= 3)
+      return fail(400, {
+        error: "The username must have more than 3 characters",
       });
     if (!acceptTOS)
       return fail(400, {
@@ -47,7 +52,7 @@ export const actions: Actions = {
 
     const { error: upsertError } = await supabase
       .from("profiles")
-      .insert({ id: newId, user_id: userId, username });
+      .insert({ id: newId, user_id: userId, username: username.toLowerCase(), display_name });
 
     if (
       upsertError?.message ===

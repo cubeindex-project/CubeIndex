38c4ce10ab45e9335d9031b61749414c6e3cc500
Saterz_ <tilouc93@gmail.com>
2025-08-14 19:30:05 -0400

    fix: improve error handling in user ratings loading
    - Changed error handling in the user ratings loading function to throw an error instead of logging it, ensuring better visibility of issues during data fetching.
    - Reformatted the select statement for clarity.

--- src/routes/(private)/user/[username]/ratings/+page.ts
+++ src/routes/(private)/user/[username]/ratings/+page.ts
@@ -6,15 +6,14 @@ export const load = (async ({ parent }) => {
 
   const { data: user_cube_ratings, error: urErr } = await supabase
     .from("user_cube_ratings")
-    .select("*, cube_model:cube_slug(*), profile:user_id(username, display_name)")
+    .select(
+      "*, cube_model:cube_slug(*), profile:user_id(username, display_name)"
+    )
     .eq("user_id", data.profile.user_id);
 
   if (urErr) {
-    console.error(`Failed to fetch user ratings: ${urErr.message}`);
-    return;
+    throw new Error(`Failed to fetch user ratings: ${urErr.message}`);
   }
-
-  console.log(user_cube_ratings);
 
   return { user_cube_ratings };
 }) satisfies PageLoad;

f320010e107f92934c777dbe5ddbbb0a350d3f08
Saterz_ <tilouc93@gmail.com>
2025-08-15 16:16:53 -0400

    refactor: remove feature toggles and clean up login, signup, and achievements pages
    - Eliminated the configCatClient and FeatureDisabled component imports from login, signup, and achievements pages to streamline the code.
    - Removed feature availability checks, simplifying the rendering logic and enhancing maintainability.
    - Cleaned up unused variables and imports across the affected files.

--- src/routes/(auth)/auth/signup/+page.svelte
+++ src/routes/(auth)/auth/signup/+page.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import { configCatClient } from "$lib/configcatClient";
-  import FeatureDisabled from "$lib/components/misc/featureDisabled.svelte";
-  import { onMount } from "svelte";
   import { enhance } from "$app/forms";
   import { SsgoiTransition } from "@ssgoi/svelte";
   import { page } from "$app/state";
@@ -13,15 +10,8 @@
   let password: string = $state("");
   let confirmPassword: string = $state("");
   let showPassword: boolean = $state(false);
-  let signup: boolean = $state(true);
   let acceptedTOS = $state(false);
   let isSubmitting = $state(false);
-
-  onMount(() =>
-    configCatClient.getValueAsync("signup", false).then((value) => {
-      signup = value;
-    })
-  );
 </script>
 
 <svelte:head>
@@ -29,185 +19,180 @@
 </svelte:head>
 
 <SsgoiTransition id={page.url.pathname}>
-  {#if signup}
-    <section
-      class="min-h-screen flex flex-col items-center justify-center px-6 relative overflow-hidden"
+  <section
+    class="min-h-screen flex flex-col items-center justify-center px-6 relative overflow-hidden"
+  >
+    <div
+      class="w-full max-w-md bg-base-200 border border-base-300 rounded-2xl shadow-lg p-8 z-10"
     >
-      <div
-        class="w-full max-w-md bg-base-200 border border-base-300 rounded-2xl shadow-lg p-8 z-10"
+      <h1 class="text-3xl font-clash font-bold text-center mb-6">
+        Join CubeIndex
+      </h1>
+      <p class="text-center text-sm mb-8">
+        Create a free account to start tracking your collection
+      </p>
+      <form
+        method="POST"
+        action="?/signup"
+        use:enhance={() => {
+          return async ({ update }) => {
+            await update({ reset: false });
+            isSubmitting = false;
+          };
+        }}
+        onsubmit={() => {
+          isSubmitting = true;
+        }}
+        class="space-y-6"
       >
-        <h1 class="text-3xl font-clash font-bold text-center mb-6">
-          Join CubeIndex
-        </h1>
-        <p class="text-center text-sm mb-8">
-          Create a free account to start tracking your collection
-        </p>
-        <form
-          method="POST"
-          action="?/signup"
-          use:enhance={() => {
-            return async ({ update }) => {
-              await update({ reset: false });
-              isSubmitting = false;
-            };
-          }}
-          onsubmit={() => {
-            isSubmitting = true;
-          }}
-          class="space-y-6"
-        >
-          <!-- Display Name -->
-          <div>
-            <label for="username" class="block text-sm font-medium">
-              Display Name
-            </label>
+        <!-- Display Name -->
+        <div>
+          <label for="username" class="block text-sm font-medium">
+            Display Name
+          </label>
+          <input
+            name="display_name"
+            type="text"
+            bind:value={display_name}
+            class="input w-full"
+            required
+          />
+        </div>
+
+        <!-- Username -->
+        <div>
+          <label for="username" class="block text-sm font-medium">
+            Username
+          </label>
+          <input
+            name="username"
+            type="text"
+            bind:value={username}
+            class="input w-full"
+            required
+          />
+        </div>
+
+        <!-- Email -->
+        <div>
+          <label for="email" class="block text-sm font-medium">Email</label>
+          <input
+            name="email"
+            type="email"
+            bind:value={email}
+            class="input w-full"
+            required
+          />
+        </div>
+
+        <!-- Password -->
+        <div>
+          <label for="password" class="block text-sm font-medium"
+            >Password</label
+          >
+          <div class="flex flex-row items-center">
             <input
-              name="display_name"
-              type="text"
-              bind:value={display_name}
+              name="password"
+              type={showPassword ? "text" : "password"}
+              bind:value={password}
               class="input w-full"
               required
             />
-          </div>
-
-          <!-- Username -->
-          <div>
-            <label for="username" class="block text-sm font-medium">
-              Username
-            </label>
-            <input
-              name="username"
-              type="text"
-              bind:value={username}
-              class="input w-full"
-              required
-            />
-          </div>
-
-          <!-- Email -->
-          <div>
-            <label for="email" class="block text-sm font-medium">Email</label>
-            <input
-              name="email"
-              type="email"
-              bind:value={email}
-              class="input w-full"
-              required
-            />
-          </div>
-
-          <!-- Password -->
-          <div>
-            <label for="password" class="block text-sm font-medium"
-              >Password</label
-            >
-            <div class="flex flex-row items-center">
+            <label class="swap text-md">
               <input
-                name="password"
-                type={showPassword ? "text" : "password"}
-                bind:value={password}
-                class="input w-full"
-                required
+                type="checkbox"
+                onclick={() => (showPassword = !showPassword)}
+                class="sr-only peer"
               />
-              <label class="swap text-md">
-                <input
-                  type="checkbox"
-                  onclick={() => (showPassword = !showPassword)}
-                  class="sr-only peer"
-                />
-                <i class="fa-solid fa-eye swap-off ml-2 cursor-pointer"></i>
-                <i class="fa-solid fa-eye-slash swap-on ml-2 cursor-pointer"
-                ></i>
-              </label>
-            </div>
-          </div>
-
-          <!-- Confirm Password -->
-          <div>
-            <label for="confirmPassword" class="block text-sm font-medium"
-              >Confirm Password</label
-            >
-            <input
-              id="confirmPassword"
-              type="password"
-              name="confirmPassword"
-              bind:value={confirmPassword}
-              class="input w-full"
-              required
-            />
-          </div>
-
-          <!-- Accept ToS and Privacy Policy -->
-          <div class="flex items-center gap-2">
-            <input
-              type="checkbox"
-              name="acceptTOS"
-              bind:checked={acceptedTOS}
-              class="checkbox bg-base-300"
-            />
-            <label for="acceptTOS" class="text-sm select-none">
-              I accept the
-              <a href="/tos" target="_blank" class="link link-primary"
-                >Terms of Service</a
-              >
-              and
-              <a href="/privacy" target="_blank" class="link link-primary"
-                >Privacy Policy</a
-              >
+              <i class="fa-solid fa-eye swap-off ml-2 cursor-pointer"></i>
+              <i class="fa-solid fa-eye-slash swap-on ml-2 cursor-pointer"></i>
             </label>
           </div>
+        </div>
 
-          <!-- Main Sign Up Button -->
-          <button
-            type="submit"
-            class="btn btn-xl w-full btn-primary"
-            disabled={isSubmitting ||
-              !email ||
-              !password ||
-              !acceptedTOS ||
-              !confirmPassword ||
-              !username ||
-              !display_name}
+        <!-- Confirm Password -->
+        <div>
+          <label for="confirmPassword" class="block text-sm font-medium"
+            >Confirm Password</label
           >
-            {#if isSubmitting}
-              <span class="loading loading-spinner"></span>
-              Signing Up...
-            {:else}
-              Sign Up
-            {/if}
-          </button>
+          <input
+            id="confirmPassword"
+            type="password"
+            name="confirmPassword"
+            bind:value={confirmPassword}
+            class="input w-full"
+            required
+          />
+        </div>
 
-          {#if form?.message}
-            <p class="text-sm text-success text-center mt-2">
-              {form.message}
-            </p>
+        <!-- Accept ToS and Privacy Policy -->
+        <div class="flex items-center gap-2">
+          <input
+            type="checkbox"
+            name="acceptTOS"
+            bind:checked={acceptedTOS}
+            class="checkbox bg-base-300"
+          />
+          <label for="acceptTOS" class="text-sm select-none">
+            I accept the
+            <a href="/tos" target="_blank" class="link link-primary"
+              >Terms of Service</a
+            >
+            and
+            <a href="/privacy" target="_blank" class="link link-primary"
+              >Privacy Policy</a
+            >
+          </label>
+        </div>
+
+        <!-- Main Sign Up Button -->
+        <button
+          type="submit"
+          class="btn btn-xl w-full btn-primary"
+          disabled={isSubmitting ||
+            !email ||
+            !password ||
+            !acceptedTOS ||
+            !confirmPassword ||
+            !username ||
+            !display_name}
+        >
+          {#if isSubmitting}
+            <span class="loading loading-spinner"></span>
+            Signing Up...
+          {:else}
+            Sign Up
           {/if}
-          {#if form?.error}
-            <p class="text-sm text-error text-center mt-2">
-              {form.error}
-            </p>
-          {/if}
+        </button>
 
-          <!-- OR Divider -->
-          <div class="divider">or</div>
+        {#if form?.message}
+          <p class="text-sm text-success text-center mt-2">
+            {form.message}
+          </p>
+        {/if}
+        {#if form?.error}
+          <p class="text-sm text-error text-center mt-2">
+            {form.error}
+          </p>
+        {/if}
 
-          <!-- Sign Up with Discord Button -->
-          <a
-            type="button"
-            href="/auth/discord"
-            class="btn btn-lg bg-[#5865F2] text-white w-full mt-4"
-          >
-            <i class="fa-brands fa-discord text-2xl"></i>
-            Sign Up with Discord
-          </a>
-        </form>
-        <p class="text-sm text-center mt-6">
-          Already have an account?
-          <a href="/auth/login" class="link link-primary link-hover">Log in</a>
-        </p>
-      </div>
-    </section>
-  {:else}
-    <FeatureDisabled featureName="Signup is" />
-  {/if}
+        <!-- OR Divider -->
+        <div class="divider">or</div>
+
+        <!-- Sign Up with Discord Button -->
+        <a
+          type="button"
+          href="/auth/discord"
+          class="btn btn-lg bg-[#5865F2] text-white w-full mt-4"
+        >
+          <i class="fa-brands fa-discord text-2xl"></i>
+          Sign Up with Discord
+        </a>
+      </form>
+      <p class="text-sm text-center mt-6">
+        Already have an account?
+        <a href="/auth/login" class="link link-primary link-hover">Log in</a>
+      </p>
+    </div>
+  </section>
 </SsgoiTransition>
--- src/routes/(public)/achievements/+page.svelte
+++ src/routes/(public)/achievements/+page.svelte
@@ -1,21 +1,9 @@
 <script lang="ts">
-  import { configCatClient } from "$lib/configcatClient";
-  import FeatureDisabled from "$lib/components/misc/featureDisabled.svelte";
-  import { onMount } from "svelte";
   import { SsgoiTransition } from "@ssgoi/svelte";
   import { page } from "$app/state";
 
   const { data } = $props();
   const { achievements } = data;
-  let achievementsAvailability: boolean = $state(true);
-
-  onMount(() =>
-    configCatClient
-      .getValueAsync("achievementsAvailability", false)
-      .then((value) => {
-        achievementsAvailability = value;
-      })
-  );
 </script>
 
 <svelte:head>
@@ -23,95 +11,91 @@
 </svelte:head>
 
 <SsgoiTransition id={page.url.pathname}>
-  {#if achievementsAvailability}
-    <section class="min-h-screen px-6 py-24">
-      <div class="max-w-6xl mx-auto">
-        <h1 class="text-4xl font-bold text-center mb-6">Achievements</h1>
-        <p class="text-center mb-12">
-          Unlock achievements by participating in the CubeIndex community.
-          Collect them all to showcase your journey!
-        </p>
+  <section class="min-h-screen px-6 py-24">
+    <div class="max-w-6xl mx-auto">
+      <h1 class="text-4xl font-bold text-center mb-6">Achievements</h1>
+      <p class="text-center mb-12">
+        Unlock achievements by participating in the CubeIndex community. Collect
+        them all to showcase your journey!
+      </p>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {#each achievements as achievement}
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {#each achievements as achievement}
+          <div
+            class="relative rounded-xl border border-base-300 p-0 shadow-md flex overflow-hidden bg-base-200"
+          >
+            <!-- Rarity Color Bar -->
             <div
-              class="relative rounded-xl border border-base-300 p-0 shadow-md flex overflow-hidden bg-base-200"
-            >
-              <!-- Rarity Color Bar -->
-              <div
-                class={"w-2 flex-shrink-0 " +
-                  (achievement.rarity == "Special"
-                    ? "bg-gradient-to-b from-yellow-400 via-pink-500 to-purple-600"
-                    : achievement.rarity == "Mythic"
-                      ? "bg-red-600"
-                      : achievement.rarity == "Legendary"
-                        ? "bg-yellow-400"
-                        : achievement.rarity == "Exotic"
-                          ? "bg-teal-400"
-                          : achievement.rarity == "Epic"
-                            ? "bg-purple-600"
-                            : achievement.rarity == "Rare"
-                              ? "bg-blue-600"
-                              : "bg-neutral-700")}
-              ></div>
-              <!-- Card Content -->
-              <div class="flex-1 p-6">
-                <!-- Icon and Rarity -->
-                <div class="flex items-center justify-between mb-4">
-                  <div class="text-4xl">{achievement.icon}</div>
-                  <span
-                    class="text-sm capitalize px-3 py-1 rounded-full font-semibold bg-base-300"
-                  >
-                    {achievement.rarity}
+              class={"w-2 flex-shrink-0 " +
+                (achievement.rarity == "Special"
+                  ? "bg-gradient-to-b from-yellow-400 via-pink-500 to-purple-600"
+                  : achievement.rarity == "Mythic"
+                    ? "bg-red-600"
+                    : achievement.rarity == "Legendary"
+                      ? "bg-yellow-400"
+                      : achievement.rarity == "Exotic"
+                        ? "bg-teal-400"
+                        : achievement.rarity == "Epic"
+                          ? "bg-purple-600"
+                          : achievement.rarity == "Rare"
+                            ? "bg-blue-600"
+                            : "bg-neutral-700")}
+            ></div>
+            <!-- Card Content -->
+            <div class="flex-1 p-6">
+              <!-- Icon and Rarity -->
+              <div class="flex items-center justify-between mb-4">
+                <div class="text-4xl">{achievement.icon}</div>
+                <span
+                  class="text-sm capitalize px-3 py-1 rounded-full font-semibold bg-base-300"
+                >
+                  {achievement.rarity}
+                </span>
+              </div>
+
+              <h2 class="text-xl font-bold mb-1">
+                {achievement.name}
+              </h2>
+              <p class="text-sm mb-2">
+                {achievement.description}
+              </p>
+
+              {#if achievement.title}
+                <div
+                  class="inline-flex items-center gap-2 rounded-full border border-accent bg-accent px-3 py-1 text-xs font-semibold tracking-wide text-accent-content mb-2"
+                >
+                  <i class="fa-regular fa-star"></i>
+                  <span>
+                    <span class="opacity-80 font-normal">Title Reward:</span>
+                    <span class="font-bold ml-1">"{achievement.title}"</span>
                   </span>
                 </div>
+              {/if}
 
-                <h2 class="text-xl font-bold mb-1">
-                  {achievement.name}
-                </h2>
-                <p class="text-sm mb-2">
-                  {achievement.description}
+              <div class="space-y-2 text-sm">
+                <p class="flex justify-between">
+                  <span class="font-semibold">Category:</span>
+                  <span>{achievement.category}</span>
                 </p>
 
-                {#if achievement.title}
-                  <div
-                    class="inline-flex items-center gap-2 rounded-full border border-accent bg-accent px-3 py-1 text-xs font-semibold tracking-wide text-accent-content mb-2"
+                <p class="flex justify-between">
+                  <span class="font-semibold">Unlock Method:</span>
+                  <span>{achievement.unlock_method}</span>
+                </p>
+
+                {#if !achievement.unlockable}
+                  <p
+                    class="inline-flex items-center gap-2 rounded-full bg-error px-3 py-1 text-xs font-bold uppercase tracking-wide text-error-content"
                   >
-                    <i class="fa-regular fa-star"></i>
-                    <span>
-                      <span class="opacity-80 font-normal">Title Reward:</span>
-                      <span class="font-bold ml-1">"{achievement.title}"</span>
-                    </span>
-                  </div>
+                    <i class="fa-solid fa-xmark"></i>
+                    Not currently unlockable
+                  </p>
                 {/if}
-
-                <div class="space-y-2 text-sm">
-                  <p class="flex justify-between">
-                    <span class="font-semibold">Category:</span>
-                    <span>{achievement.category}</span>
-                  </p>
-
-                  <p class="flex justify-between">
-                    <span class="font-semibold">Unlock Method:</span>
-                    <span>{achievement.unlock_method}</span>
-                  </p>
-
-                  {#if !achievement.unlockable}
-                    <p
-                      class="inline-flex items-center gap-2 rounded-full bg-error px-3 py-1 text-xs font-bold uppercase tracking-wide text-error-content"
-                    >
-                      <i class="fa-solid fa-xmark"></i>
-                      Not currently unlockable
-                    </p>
-                  {/if}
-                </div>
               </div>
             </div>
-          {/each}
-        </div>
+          </div>
+        {/each}
       </div>
-    </section>
-  {:else}
-    <FeatureDisabled featureName="Achievements are" />
-  {/if}
+    </div>
+  </section>
 </SsgoiTransition>

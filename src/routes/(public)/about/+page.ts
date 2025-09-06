1e1d702fdb7323f41213fb7679649299ccab68b5
Saterz_ <tilouc93@gmail.com>
2025-09-06 12:06:06 +0100

    feat: refactor about page to include features and future features with improved UI handling

--- src/routes/(public)/about/+page.ts
+++ src/routes/(public)/about/+page.ts
@@ -4,23 +4,30 @@ import type { Profiles } from "$lib/components/dbTableTypes";
 import { supabase } from "$lib/supabaseClient";
 
 export const load = (async ({ setHeaders }) => {
-  const { data: profiles, error: err } = await supabase
+  const { data: team, error: err } = await supabase
     .from("profiles")
-    .select("*");
+    .select("*")
+    .neq("role", "User")
+    .neq("username", "cubeindex");
 
   if (err) throw error(500, err.message);
 
-  const team = profiles.filter(
-    (p: Profiles) => p.role !== "User" && p.username !== "cubeIndex"
-  );
-
-  const logoDesigner = profiles.find(
+  const logoDesigner = team.find(
     (p: Profiles) => p.user_id === "b49da5bb-6d82-463e-b8ee-fd7c9feebde6"
   );
+
+  const { data: features, error: fErr } = await supabase
+    .from("features")
+    .select("*");
+
+  if (fErr) throw error(500, fErr.message);
+
+  const currentFeatures = features.filter((f) => f.implemented === true);
+  const futureFeatures = features.filter((f) => f.implemented === false);
 
   setHeaders({
     "Cache-Control": "public, s-maxage=600, stale-while-revalidate=86400",
   });
 
-  return { profiles, logoDesigner, team };
+  return { logoDesigner, team, features: currentFeatures, futureFeatures };
 }) satisfies PageLoad;

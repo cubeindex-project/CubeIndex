9802934419370c66223a838d2bb19306f6d7da71
Saterz_ <tilouc93@gmail.com>
2025-09-14 11:28:21 +0100

    feat: implement server-side layout redirection for non-authenticated users in private routes

---
+++ src/routes/(private)/+layout.server.ts
@@ -0,0 +1,6 @@
+import { redirect } from "@sveltejs/kit";
+import type { LayoutServerLoad } from "./$types";
+
+export const load = (async ({ locals: { user } }) => {
+  if (!user) throw redirect(302, "/auth/login");
+}) satisfies LayoutServerLoad;

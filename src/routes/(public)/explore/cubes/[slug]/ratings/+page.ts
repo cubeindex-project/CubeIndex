fec5ad00bced64ce5577eebf0c1f4b74dda6025f
Saterz <tilouc93@gmail.com>
2026-01-10 19:05:46 +0100

    refactor: improve fetch in cube details page

--- src/routes/(public)/explore/cubes/[slug]/ratings/+page.ts
+++ src/routes/(public)/explore/cubes/[slug]/ratings/+page.ts
@@ -1,39 +1,18 @@
 import type { PageLoad } from "./$types";
-import type { DetailedCube } from "$lib/components/dbTableTypes";
 import { clientLogError } from "$lib/logger/clientLogError";
 import { clientLogger } from "$lib/logger/client";
 
 export const load = (async ({ parent, params }) => {
-  const { supabase } = await parent();
+  const { supabase, cube } = await parent();
   const { slug } = params;
-
-  const cubePromise = supabase
-    .from("v_detailed_cube_models")
-    .select("*")
-    .eq("slug", slug)
-    .single();
 
   const ratingsPromise = supabase
     .from("user_cube_ratings")
     .select("*, profile:user_id(username, display_name)")
     .eq("cube_slug", slug);
 
-  const [cubeRes, ratingsRes] = await Promise.all([
-    cubePromise,
-    ratingsPromise,
-  ]);
+  const [ratingsRes] = await Promise.all([ratingsPromise]);
 
-  const cube = cubeRes.data as DetailedCube | null;
-
-  if (!cube) {
-    return clientLogError(
-      "Cube not found",
-      clientLogger,
-      new Error(`Cube "${slug}" not found`),
-      true,
-      404,
-    );
-  }
   if (ratingsRes.error) {
     return clientLogError(
       "Unable to load cube ratings",

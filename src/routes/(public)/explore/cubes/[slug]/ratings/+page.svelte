68f4dd810f900f2b8d719b4bf6d64b086b0cd917
Saterz <tilouc93@gmail.com>
2026-06-17 18:19:35 +0100

    refactor: simplify data destructuring in ratings component

--- src/routes/(public)/explore/cubes/[slug]/ratings/+page.svelte
+++ src/routes/(public)/explore/cubes/[slug]/ratings/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
   import UserRatings from "$lib/components/rating/userRatings.svelte";
-  import type { Tables } from "$lib/types/database.types";
 
   let { data } = $props();
-  let { cube = {} as Tables<"v_detailed_cube_models">, user_cube_ratings = [] as any[] } =
-    $derived(data);
+  let { cube, user_cube_ratings } = $derived(data);
 </script>
 
 <UserRatings {user_cube_ratings} {cube} />

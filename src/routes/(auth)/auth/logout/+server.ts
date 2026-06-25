ebc0629d438d35742f4da100a57673e5cb4ce397
Saterz <tilouc93@gmail.com>
2026-06-25 18:13:34 +0100

    feat: add scope support for the signout route through the url params

--- src/routes/(auth)/auth/logout/+server.ts
+++ src/routes/(auth)/auth/logout/+server.ts
@@ -1,7 +1,20 @@
 import { redirect } from "@sveltejs/kit";
+import { SIGN_OUT_SCOPES, type SignOutScope } from "@supabase/supabase-js";
+import { logError } from "$lib/server/logError.js";
 
-export const GET = async ({ locals: { supabase } }) => {
-  await supabase.auth.signOut();
+export const GET = async ({ locals: { supabase, log }, url }) => {
+  const scope = url.searchParams.get("scope");
+
+  if (scope && !SIGN_OUT_SCOPES.includes(scope as SignOutScope)) {
+    throw logError(
+      400,
+      "The scope is not correct",
+      log,
+      new Error("The scope is not correct"),
+    );
+  }
+
+  await supabase.auth.signOut({ scope: (scope ?? undefined) as SignOutScope });
 
   redirect(307, "/");
 };

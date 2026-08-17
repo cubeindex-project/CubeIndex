ec3e19509a802179dc023b695407d1cad94960b1
Saterz <tilouc93@gmail.com>
2026-08-16 20:02:57 -0400

    refactor: use modal state for YouTube video card

--- src/lib/components/misc/youtubeVideoCard.svelte
+++ src/lib/components/misc/youtubeVideoCard.svelte
@@ -1,15 +1,36 @@
 <script lang="ts">
-  import Card from "../layout/card.svelte";
-  const { title = "YouTube video", videoUrl, onCancel } = $props();
+  import Modal from "$lib/components/ui/Modal.svelte";
+
+  interface Props {
+    open: boolean;
+    title?: string;
+    videoURL: string;
+  }
+
+  let {
+    open = $bindable(),
+    title = "YouTube video",
+    videoURL,
+  }: Props = $props();
 </script>
 
-<Card {onCancel} {title}>
+<Modal bind:open {title}>
   <iframe
-    class="aspect-video w-[560px] max-w-full rounded-2xl"
-    src={videoUrl}
+    class="aspect-video w-full rounded-2xl"
+    src={videoURL}
     {title}
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     referrerpolicy="strict-origin-when-cross-origin"
     allowfullscreen
   ></iframe>
-</Card>
+
+  <div class="modal-action sm:justify-end">
+    <button
+      type="button"
+      class="btn flex-1 sm:flex-none"
+      onclick={() => (open = false)}
+    >
+      Close
+    </button>
+  </div>
+</Modal>

55e0c9a0ca69c133b2a96ede732218115701fd80
Saterz <tilouc93@gmail.com>
2026-08-22 16:27:26 -0400

    fix: update cube model types

--- src/lib/types/database.types.ts
+++ src/lib/types/database.types.ts
@@ -70,7 +70,7 @@ export type Database = MergeDeep<
         };
         v_detailed_cube_models: {
           Row: WithReleaseDate<
-            DatabaseGenerated["public"]["Views"]["v_detailed_cube_models"]["Row"]
+            DatabaseGenerated["public"]["Tables"]["cube_models"]["Row"]
           >;
         };
         v_achievement_rarity: {

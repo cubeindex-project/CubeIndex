9396c9bc232da925c6d04e7be6cd833ca2260b43
Saterz <tilouc93@gmail.com>
2026-08-22 15:52:19 -0400

    fix: formatting in CubeVersionType

--- src/lib/components/cube/CubeVersionType.svelte
+++ src/lib/components/cube/CubeVersionType.svelte
@@ -3,15 +3,11 @@
 </script>
 
 {#if version_type === "Base"}
-  <span
-    class="badge badge-primary text-primary-content text-xs font-semibold"
-  >
+  <span class="badge badge-primary text-primary-content text-xs font-semibold">
     <span class="font-clash uppercase">Base</span>
   </span>
 {:else if version_type === "Limited"}
-  <span
-    class="badge badge-accent text-accent-content text-xs font-semibold"
-  >
+  <span class="badge badge-accent text-accent-content text-xs font-semibold">
     <span class="font-clash uppercase">Limited Edition</span>
   </span>
 {:else}
